76b6746360c336b98a613ed7cd846419fb5bb46c
reshad21 <miakala7@gmail.com>
2024-07-12 02:30:07 +0600

    swiper slider add

--- src/components/Home/HeroSlider.tsx
+++ src/components/Home/HeroSlider.tsx
@@ -1,63 +1,40 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+// import required modules
+import { Autoplay } from "swiper/modules";
+
+import img1 from "../../assets/Images/slider1.jpg";
+import img2 from "../../assets/Images/slider2.jpg";
+import img3 from "../../assets/Images/slider3.jpg";
+import img4 from "../../assets/Images/slider4.jpg";
+
 const HeroSlider = () => {
   return (
-    <div className="carousel w-full my-7 border-2 border-slate-700 rounded-xl">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-    </div>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <img src={img1} className="w-full h-[400px] object-fill" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img2} className="w-full h-[400px] object-fill" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img3} className="w-full h-[400px] object-fill" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img4} className="w-full h-[400px] object-fill" />
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
