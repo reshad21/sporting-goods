8fe8c72f072c71b45e446abeba190dbe0c253ef3
reshad21 <miakala7@gmail.com>
2024-07-12 16:40:45 +0600

    filter is not work proeprly

--- src/redux/features/Product/productApi.ts
+++ src/redux/features/Product/productApi.ts
@@ -39,14 +39,22 @@ const productApi = baseApi.injectEndpoints({
         getAllFilterProducts: builder.query({
             query: ({ category, brand, rating, price }) => {
                 console.log("RTK QUERY = >", category, brand, rating, price);
-                let queryStr = `products?`;
-                if (category) queryStr += `category=${category}&`;
-                if (brand) queryStr += `brand=${brand}&`;
-                if (rating) queryStr += `rating=${rating}&`;
-                if (price) queryStr += `price=${price}&`;
-                return queryStr.slice(0, -1);
-            },
+
+                // Using URLSearchParams to construct query string
+                const params = new URLSearchParams();
+
+                if (category) params.append('category', category);
+                if (brand) params.append('brand', brand);
+                if (rating !== null) params.append('rating', rating.toString());
+                if (price !== null) params.append('price', price.toString());
+
+                return {
+                    url: `/products?${params.toString()}`,
+                    method: 'GET'
+                };
+            }
         }),
+
         searchProducts: builder.query({
             query: (searchTerm) => {
                 const params = new URLSearchParams();
