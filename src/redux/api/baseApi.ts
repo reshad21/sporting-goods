41d6fa4db18c736d97fe59f63dbed268e104336e
reshad21 <miakala7@gmail.com>
2024-08-23 11:32:31 +0600

    base api change

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    // baseQuery: fetchBaseQuery({ baseUrl: 'https://sporting-goods-db.vercel.app' }),
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://sporting-goods-db.vercel.app' }),
+    // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
     tagTypes: ['sports'],
     endpoints: () => ({}),
 });
